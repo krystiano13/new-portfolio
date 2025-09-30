908dd96e01764b33c268c83a6f693651eedbcb28
Krystian Zieja <60575093+krystiano13@users.noreply.github.com>
2025-09-30 14:20:37 +0200

    create page's front

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "./assets/logo1.webp"
+import oktalLogo from "./assets/oktal_labs_logo.jpeg"
+import uniLogo from "./assets/pw.jpeg"
+import schoolLogo from "./assets/mechaniak.jpeg"
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="wrapper">
+            <h1 className="text-4xl font-bold mb-6">Hi ! I'm Krystian 🖐️️</h1>
+            <h2 className="text-2xl font-semibold mb-6">A Software Engineer</h2>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <h3 className="text-xl font-medium mb-3">
+                ABOUT ME
+            </h3>
+            <p className="text-justify text-gray-800 mb-6">
+                I am a software engineer that specializes in building web applications
+                with usage of technologies such as React, PHP and Vue. I am also an active
+                student in Warsaw University of Technology, where I am pursuing a bachelor's
+                degree in Automation, Robotics and Industrial Computer Science. Outside of the job
+                and college, I am a hobbist indie game developer.
+                I also enjoy variety of physical activities such as running, weightlifting
+            </p>
+
+            <h3 className="text-xl font-medium mb-3">
+                WORK EXPERIENCE
+            </h3>
+            <section className="mb-6">
+                <div className="border-1 border-gray-200 p-4 rounded-lg mb-3">
+                    <div className="flex justify-between">
+                        <div className="flex items-center">
+                            <img
+                                className="rounded-full border-1 border-gray-200 w-16 h-16"
+                                src={oktalLogo as string}
+                                alt="Oktal Labs logo"
+                            />
+                            <div className="ml-4">
+                                <h4 className="font-semibold text-lg">Fullstack Developer</h4>
+                                <h5>Oktal Labs</h5>
+                            </div>
+                        </div>
+                        <div className="text-gray-500">Feb 2025 - Now</div>
+                    </div>
+                </div>
+                <div className="border-1 border-gray-200 p-4 rounded-lg">
+                    <div className="flex justify-between">
+                        <div className="flex items-center">
+                            <img
+                                className="rounded-full border-1 border-gray-200 p-4 w-16 h-16"
+                                src={logo as string}
+                                alt="Timenotes logo"
+                            />
+                            <div className="ml-4">
+                                <h4 className="font-semibold text-lg">Junior React Developer</h4>
+                                <h5>Timenotes.io</h5>
+                            </div>
+                        </div>
+                        <div className="text-gray-500">Aug 2024 - Oct 2024</div>
+                    </div>
+                </div>
+            </section>
+
+            <h3 className="text-xl font-medium mb-3">
+                EDUCATION
+            </h3>
+            <section className="mb-6">
+                <div className="border-1 border-gray-200 p-4 rounded-lg mb-3">
+                    <div className="flex justify-between">
+                        <div className="flex items-center">
+                            <img
+                                className="rounded-full border-1 border-gray-200 p-2 w-16 h-16"
+                                src={uniLogo as string}
+                                alt="Warsaw University of Technology logo"
+                            />
+                            <div className="ml-4">
+                                <h4 className="font-semibold text-lg">Warsaw University of Technology</h4>
+                                <h5>Automation, Robotics and Industrial Computer Science</h5>
+                            </div>
+                        </div>
+                        <div className="text-gray-500">Oct 2023 - Now</div>
+                    </div>
+                </div>
+                <div className="border-1 border-gray-200 p-4 rounded-lg mb-3">
+                    <div className="flex justify-between">
+                        <div className="flex items-center">
+                            <img
+                                className="rounded-full border-1 border-gray-200 p-2 w-16 h-16"
+                                src={schoolLogo as string}
+                                alt="My High School logo"
+                            />
+                            <div className="ml-4">
+                                <h4 className="font-semibold text-lg">
+                                    ZSMIO nr.5 w Łomży
+                                </h4>
+                                <h5>
+                                    IT technician
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="text-gray-500">Sep 2019 - Apr 2023</div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default App
