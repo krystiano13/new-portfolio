5b4d3d56f488ae4db635ba2409641c3177970aed
Krystian Zieja <60575093+krystiano13@users.noreply.github.com>
2025-09-30 14:35:55 +0200

    create AboutMe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+// components
+import { AboutMe } from "./components/about-me/AboutMe.tsx";
 import { ExperienceSection } from "./components/experience-section/ExperienceSection.tsx";
+
+// data
 import { workSection } from "./components/experience-section/work-section.ts";
 import { educationSection } from "./components/experience-section/education-section.ts";
 
@@ -8,17 +12,7 @@ function App() {
             <h1 className="text-4xl font-bold mb-6">Hi ! I'm Krystian 🖐️️</h1>
             <h2 className="text-2xl font-semibold mb-6">A Software Engineer</h2>
 
-            <h3 className="text-xl font-medium mb-3">
-                ABOUT ME
-            </h3>
-            <p className="text-justify text-gray-800 mb-6">
-                I am a software engineer that specializes in building web applications
-                with usage of technologies such as React, PHP and Vue. I am also an active
-                student in Warsaw University of Technology, where I am pursuing a bachelor's
-                degree in Automation, Robotics and Industrial Computer Science. Outside of the job
-                and college, I am a hobbist indie game developer.
-                I also enjoy variety of physical activities such as running, weightlifting
-            </p>
+            <AboutMe />
 
             {
                 [workSection, educationSection].map(item => (
---
+++ src/components/about-me/AboutMe.tsx
@@ -0,0 +1,17 @@
+export function AboutMe() {
+    return (
+        <>
+            <h3 className="text-xl font-medium mb-3">
+                ABOUT ME
+            </h3>
+            <p className="text-justify text-gray-800 mb-6">
+                I am a software engineer that specializes in building web applications
+                with usage of technologies such as React, PHP and Vue. I am also an active
+                student in Warsaw University of Technology, where I am pursuing a bachelor's
+                degree in Automation, Robotics and Industrial Computer Science. Outside of the job
+                and college, I am a hobbist indie game developer.
+                I also enjoy variety of physical activities such as running, weightlifting
+            </p>
+        </>
+    )
+}
