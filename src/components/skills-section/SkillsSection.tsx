a294bcbbed8c010f536ef14be805138a8d4cc6a3
Krystian Zieja <60575093+krystiano13@users.noreply.github.com>
2025-10-07 19:11:34 +0200

    implement skills section

--- src/components/skills-section/SkillsSection.tsx
+++ src/components/skills-section/SkillsSection.tsx
@@ -8,10 +8,10 @@ export function SkillsSection()
             <h3 className={`text-xl font-medium mb-3`}>
                 SKILLS
             </h3>
-            <div className="flex flex-wrap gap-1">
+            <div className="w-full flex flex-wrap gap-1">
                 {
                     skills.map(item => (
-                        <div className="p-6 border-1 border-gray-200 rounded-lg w-22 flex flex-col items-center gap-2 justify-center">
+                        <div style={{ flex: "1 1 0px" }} className="p-6 border-1 border-gray-200 rounded-lg w-22 flex flex-col items-center gap-2 justify-center">
                             <i
                                 className={`w-full text-center devicon-${item.iconName}-plain text-3xl`}
                             ></i>
