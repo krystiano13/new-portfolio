25e64b56565ace8d97105af5aafa8d90b37f0c2a
Krystian Zieja <60575093+krystiano13@users.noreply.github.com>
2025-10-08 17:56:43 +0200

    improve contact section

--- src/components/contact-section/ContactSection.tsx
+++ src/components/contact-section/ContactSection.tsx
@@ -10,18 +10,25 @@ export function ContactSection()
             <p className="dark:text-gray-200">
                 You can contact me either via {" "}
                 <a
-                    className="text-blue-500"
+                    className="text-blue-500 hover:text-blue-600 transition"
                     href="https://www.linkedin.com/in/krystian-zieja-1b9902253/"
                 >
-                    Linkedin
+                    LINKEDIN
                 </a>
                 {" "} or {" "}
                 <a
-                    className="text-blue-500"
+                    className="text-blue-500 hover:text-blue-600 transition"
                     href="mailto:crystianotv@gmail.com"
                 >
-                    Email
+                    EMAIL
                 </a>.
+                You can also check out my {" "}
+                <a
+                    className="text-blue-500 hover:text-blue-600 transition"
+                    href="https://github.com/krystiano13"
+                >
+                    GITHUB PROFILE
+                </a>
             </p>
         </Fragment>
     )
