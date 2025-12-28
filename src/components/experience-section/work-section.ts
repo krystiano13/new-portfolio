aefcb84caf6f38b5376a270c82d8d85c24b20279
krystiano13 <crystianotv@gmail.com>
2025-12-28 21:06:34 +0100

    add new company in work section

--- src/components/experience-section/work-section.ts
+++ src/components/experience-section/work-section.ts
@@ -1,33 +1,45 @@
-import logo from "../../assets/logo1.webp"
-import oktalLogo from "../../assets/oktal_labs_logo.jpeg"
+import logo from "../../assets/logo1.webp";
+import oktalLogo from "../../assets/oktal_labs_logo.jpeg";
+import innovationLogo from "../../assets/innovation.jpg";
 
 import type { Section } from "./types/section.ts";
 
 export const workSection: Section = {
-    title: "WORK EXPERIENCE",
-    elements: [
-        {
-            title: "Fullstack Developer",
-            subtitle: "Oktal Labs",
-            date: "Feb 2025 - Now",
-            logoSrc: oktalLogo as string,
-            logoAlt: "Oktal Labs logo",
-            description: "I worked on a CRM system designed to automate the creation of VAT and excise tax refund" +
-                " applications. I also contributed to the development of the portal znajdzkorty.pl and was" +
-                " involved in modernizing the company profile website for Nova Instalacje. Additionally, I" +
-                " worked on the backend of an order management system built with Symfony, as well as a" +
-                " dedicated mobile application integrated with this system. I also participated in several" +
-                " smaller external projects, including the development of custom modules and the" +
-                " maintenance of existing web applications."
-        },
-        {
-            title: "Junior React Developer",
-            subtitle: "Timenotes.io",
-            date: "Aug 2024 - Oct 2024",
-            logoSrc: logo as string,
-            logoAlt: "Timenotes.io logo",
-            description: "Creating new Timenotes' landing page with Next.js, TailwindCSS and Sanity.io " +
-                "Working on Timenotes' React app - time tracking software for teams"
-        },
-    ]
+  title: "WORK EXPERIENCE",
+  elements: [
+    {
+      title: "Software Developer",
+      subtitle: "Innovation Software",
+      date: "Oct 2025 - Now",
+      logoSrc: innovationLogo as string,
+      logoAlt: "Innovation Software logo",
+      description: "",
+    },
+    {
+      title: "Fullstack Developer",
+      subtitle: "Oktal Labs",
+      date: "Feb 2025 - Now",
+      logoSrc: oktalLogo as string,
+      logoAlt: "Oktal Labs logo",
+      description:
+        "I worked on a CRM system designed to automate the creation of VAT and excise tax refund" +
+        " applications. I also contributed to the development of the portal znajdzkorty.pl and was" +
+        " involved in modernizing the company profile website for Nova Instalacje. Additionally, I" +
+        " worked on the backend of an order management system built with Symfony, as well as a" +
+        " dedicated mobile application integrated with this system. I also participated in several" +
+        " smaller external projects, including the development of custom modules and the" +
+        " maintenance of existing web applications.",
+    },
+    {
+      title: "Junior React Developer",
+      subtitle: "Timenotes.io",
+      date: "Aug 2024 - Oct 2024",
+      logoSrc: logo as string,
+      logoAlt: "Timenotes.io logo",
+      description:
+        "Creating new Timenotes' landing page with Next.js, TailwindCSS and Sanity.io " +
+        "Working on Timenotes' React app - time tracking software for teams",
+    },
+  ],
 };
+
